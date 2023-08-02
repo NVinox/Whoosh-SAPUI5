1fa8f3a9fe6e2b4222ff1dbee428f23ff679208d
TailssTF <tailss5@mail.ru>
2023-08-02 15:19:02 +0700

    feat: верстка Express-Delivery

--- webapp/core/model/App.js
+++ webapp/core/model/App.js
@@ -47,6 +47,24 @@ sap.ui.define(["App/base/BaseModel"], (BaseModel) => {
           text: "The blog",
         },
       ],
+      expressPads: [
+        {
+          title: "Become a Courier",
+          icon: "/assets/images/courier.svg",
+          icon2: "/assets/images/route.svg",
+          text: "You choose a schedule. You decide how much and when to earn. Earnings from day one",
+        },
+        {
+          title: "Help & Support",
+          icon: "/assets/images/support.svg",
+          text: "Door-to-door delivery \nin 90 minutes or at your convenience",
+        },
+        {
+          title: "Affiliate Program",
+          icon: "/assets/images/case.svg",
+          text: "Use our service and feel \nnew quality of this traditional service!",
+        },
+      ],
     }),
   };
 });
